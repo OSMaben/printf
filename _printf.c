6c1e53481f4c615515c0e794aade88d23af6d958
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-23 13:26:18 -0700

    trying again

--- _printf.c
+++ _printf.c
@@ -1,67 +1,61 @@
 #include "main.h"
-
-int _printf(const char *format, ...)
+int  my_printf(const char *format, ...)
 {
+	unsigned h = 0, r_value = 0;
 	va_list args;
 	va_start(args, format);
 
-	int count = 0;
-
-	while (*format != '\0')
+	for ( ; format[h] != '\0' ; h++)
 	{
-		if (*format != '%')
+		if (format[h] != '%')
 		{
-			_putchar(*format);
-			count++;
-			format++;
+			_putchar(format[h]);
 		}
-		else
+		else if (format[h+1] == 'c')
 		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						_putchar(c);
-						count++;
-						break;
-					}
+			_putchar(va_arg(args, int));
+			h++;
+		}
+		else if (format[h+1] == 's')
+		{
+			int r_val = put_s(va_arg(args, char *));
+			h++;
+			r_value += (r_val - 1);
+		}
+		else if (format[h+1] == '%')
+		{
+			_putchar('%');
+			h++;
+		}
+		r_value += 1;
+	}
+	return (r_value);
+}
 
-				case 's':
-					{
-						char *s = va_arg(args, char *);
-						while (*s != '\0')
-						{
-							_putchar(*s);
-							count++;
-							s++;
-						}
-						break;
-					}
 
-				case '%':
-					{
-						_putchar('%');
-						count++;
-						break;
-					}
 
-				default:
-					{
-						_putchar('%');
-						count++;
-						_putchar(*format);
-						count++;
-						break;
-					}
 
-			}
-			format++;
+int put_s(char *string)
+{
+	int idx = 0, r_val = 0;
+
+	if (string)
+	{
+		while (string[idx] != '\0')
+		{
+			_putchar(string[idx]);
+			r_val += 1;
+			idx++;
 		}
 	}
-
-	va_end(args);
-	return count;
+	return (r_val);
 }
+
+
+
+int _putchar(char c)
+{
+	    return (write(1, &c, 1));
+}
+
 
--- main.h
+++ main.h
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stddef.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int put_s(char *string);
 
 #endif
